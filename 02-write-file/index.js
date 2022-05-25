b9144c9ea76a040eab7dd4c4dc111d2b2d7ec666
MedakD <timemedak@gmail.com>
2022-05-25 13:39:50 +0300

    init: task №3 and №4

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,40 +1,13 @@
-const fs = require("fs");
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = require('process');
 
-+function askPrompt(query) {
-    return new Promise(resolve => rl.question(query, ans => {
-        resolve(ans);
-    }))
-    }
-    
-    const writeCustomContent = async function() {
-    const content = await askPrompt("Please enter content:");
-    
-    fs.writeFile('./02-write-file/file.txt', content, err => {
-        if (err) {
-        console.error(err);
-        }
-    });
-    };
-    
-    rl.on('line', (input) => {
-    if (input === 'exit') {
-        rl.close()
-    }
-    fs.appendFile('./02-write-file/file.txt', `\n${input}`, err => {
-        if (err) {
-        console.error(err);
-        }
-    });
-});
-
-rl.on('close', function () {
-    console.log('\nFile created! Have a nice day :)!');
-  process.exit(0);
-});
-
-writeCustomContent();
+const streamWrite = new fs.WriteStream(path.join(__dirname, 'text.txt'))
+stdout.write('Hi, write something! \n')
+process.on('SIGINT', () => process.exit());
+stdin.on('data', (data)=> {
+  if(data.toString().trim()==='exit') process.exit();
+  streamWrite.write(data.toString());
+})
+process.on('SIGINT', () => process.exit());
+process.on('exit', ()=> stdout.write('Bye-Bye!'))
